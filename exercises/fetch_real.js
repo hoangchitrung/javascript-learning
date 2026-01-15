95588d407c8911ad0cdaa26dc079f99c3d30c55d
hoangchitrung <trunghoang1124@gmail.com>
2026-01-15 22:20:20 +0700

    Add fetchPosts function to retrieve and display top 3 posts

---
+++ exercises/fetch_real.js
@@ -0,0 +1,36 @@
+/*
+    Take top 3 posts
+    Print on the screen with format 
+
+    Title: ... (all uppercase)
+    Content: ...
+    -------------------------
+*/
+
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+const fetchPosts = async () => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Error while fetching API")
+        }
+
+        // Transfer from text into object
+        const posts = await response.json();
+        console.log(posts.length);
+
+        for (let i = 0; i < 3; i++) {
+            const post = posts[i];
+            console.log(`Title: ${post.title.toUpperCase()}`);
+            console.log(`Content: ${post.body}`);
+            console.log("-------------------------------------");
+            
+        }
+    } catch (error) {
+        console.log("Error: ", error.message);
+    }
+}
+
+fetchPosts();
