550525c9c3aa805f4e4230be9aacd4489fe9a4db
hoangchitrung <trunghoang1124@gmail.com>
2026-01-21 10:41:05 +0700

    very basic DOM for todo app

---
+++ schools/day2/script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const list = document.getElementById('taskList');
+
+addBtn.addEventListener('click', () => {
+    const text = input.value;
+    if (!text) { // nếu input field trống thì sẽ alert
+        alert("Don't leave the field empty!");
+        return;
+    }
+
+    // element li để hiển thị task ở phía dưới
+    const newTask = document.createElement("li");
+    newTask.textContent = text;
+    const removeBtn = document.createElement("button");
+    newTask.classList.add("list");
+    // nút xóa
+    removeBtn.textContent = "🗑️";
+    removeBtn.style.margin = "8px";
+    removeBtn.onclick = () => newTask.remove();
+    // chèn child elements vào bên trong
+    newTask.appendChild(removeBtn);
+    list.appendChild(newTask);
+});
+
+
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') addBtn.click();
+})
